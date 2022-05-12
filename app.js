7d2bbf5ffbf70cb90ac0831e14cbf38095dbe439
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-05-12 19:25:44 +0530

    updated

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var username = prompt("give me username")
-alert("nice username " + username)
+var btnTranslate = document.querySelector("#btn-translate");
+console.log("hello world")
+console.log(btnTranslate)
+
+ btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked!")
+}) 
